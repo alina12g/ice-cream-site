a90ab4f18f1d8346fd2d76102f6e000af5f1d460
Alina Negru <alina12g@yahoo.com>
2021-11-18 13:38:28 +0200

    nav and bannerCards added

--- ice-cream-site/src/App.js
+++ ice-cream-site/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p> Ice Cream App </p>
+        <p> Ice Cream App! </p>
       </header>
     </div>
   );
---
+++ ice-cream-site/src/components/Navbar/MenuItemsRight.js
@@ -0,0 +1,15 @@
+// import UserIcon from "../../images/UserIcon.png";
+// import BasketIcon from "../../images/BasketIcon.png";
+
+// export const MenuItemsRight = [
+//     {
+//         title : 'Account',
+//         img : UserIcon,
+//         cName : 'nav-links'
+//     },
+//     {
+//         title : 'Basket',
+//         img : BasketIcon,
+//         cName : 'nav-links'
+//     }
+// ];
